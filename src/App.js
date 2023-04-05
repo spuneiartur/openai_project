bdc64f4ca78dc37f316e57deb2b518896abc009f
Spunei Artur <arthurspunei@gmail.com>
2023-04-05 15:32:34 +0300

    Commit after 2 merged input and chatChistory

--- src/App.js
+++ src/App.js
@@ -5,28 +5,11 @@ import "./Input.css";
 import ChatHistory from "./ChatHistory";
 import "./ChatHistory.css";
 
-
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>C.R.A.S.T. Hello World</p>
-        <Input></Input>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-          
-        </a>
-      </header>
-      <ChatHistory>
-          
-      </ChatHistory>  
+      <ChatHistory></ChatHistory>
+      <Input></Input>
     </div>
   );
 }
