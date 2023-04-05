206595de8d7a8bb717ef29b708f28836b0e7cb31
theeacx <licxandruteodora21@stud.ase.ro>
2023-04-05 22:27:44 +0300

    header gen

--- src/Header.js
+++ src/Header.js
@@ -1,8 +1,12 @@
+import logoCrast from "./img/logo-no-background.png";
 function Header() {
     return (
         <div className="header">
-            <h1>C.R.A.S.T. zone</h1>
-            <p>Let's play some FAZAN!</p>
+            <div className ="header__logo--container">
+                <img src={logoCrast} alt="logo-no-backgroud.png" border="0"></img>
+            </div>
+            {/* <h1>C.R.A.S.T. zone</h1>
+            <p>Let's play some FAZAN!</p> */}
         </div>
     )
 }
