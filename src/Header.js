6cad863b47ab73b2a0205e8f5cefc27aea4cf16a
theeacx <licxandruteodora21@stud.ase.ro>
2023-04-05 11:25:08 +0300

    header-branch

---
+++ src/Header.js
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="header">
+            <h1>C.R.A.S.T zone</h1>
+            <p>Let's play some FAZAN!</p>
+        </div>
+    )
+}
+
+export default Header;
