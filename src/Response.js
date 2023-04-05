94c990d62f8c255652513908743c2854b31838fa
Spunei Artur <arthurspunei@gmail.com>
2023-04-05 19:43:19 +0300

    Styled Response component

--- src/Response.js
+++ src/Response.js
@@ -1,5 +1,18 @@
+//import "react";
+import './Response.css';
+import userIcon from './img/user_icon.png';
+import aiIcon from './img/ai_icon.png';
 function Response(message) {
-  return <div>Hello</div>;
+  return (
+    <div className="response__container">
+      <div className="response__icon">
+        <img src={'user' == 'user' ? userIcon : aiIcon}></img>
+      </div>
+      <div className="response__content">
+        <p>Message</p>
+      </div>
+    </div>
+  );
 }
 
 class Message {
