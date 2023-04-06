f4dc477a14cd6924a9d82e33aeb68d048c5e590c
Spunei Artur <arthurspunei@gmail.com>
2023-04-06 10:42:20 +0300

    merged header and error message components + style

--- src/App.js
+++ src/App.js
@@ -1,27 +1,22 @@
-
-import Header from "./Header";
-import "./Header.css"
+import Header from './Header';
+import './Header.css';
 import logo from './logo.svg';
 import './App.css';
 import Response from './Response';
 import controller from './modules/controller';
-import ChatHistory from "./ChatHistory";
-import "./ChatHistory.css";
-
+import ChatHistory from './ChatHistory';
+import './ChatHistory.css';
 
 function App() {
-  const myInput = 'mere bune';
-  const previous = ['pere', 'mere', 'repede'];
-  const letters = myInput.slice(-2);
-  try{
-    controller.validateInput(myInput,previous,letters);
-    console.log('Corect!');
-  }catch(err){
-    console.error(err.message);
-  }
-
-
-
+  // const myInput = 'mere bune';
+  // const previous = ['pere', 'mere', 'repede'];
+  // const letters = myInput.slice(-2);
+  // try {
+  //   controller.validateInput(myInput, previous, letters);
+  //   console.log('Corect!');
+  // } catch (err) {
+  //   console.error(err.message);
+  // }
 
   function inputHandlerSubmit(value) {
     console.log(value);
@@ -29,9 +24,8 @@ function App() {
 
   return (
     <div className="App">
-
+      <Header />
       <ChatHistory inputHandlerSubmit={inputHandlerSubmit}></ChatHistory>
-
     </div>
   );
 }
