4842f3f8c82bafa2aeb1b11690308866041786bd
Spunei Artur <arthurspunei@gmail.com>
2023-04-06 10:30:24 +0300

    added event handler on input component

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import Response from './Response';
 import controller from './modules/controller';
 
-import ChatHistory from "./ChatHistory";
-import "./ChatHistory.css";
+import ChatHistory from './ChatHistory';
+import './ChatHistory.css';
 
 function App() {
+  function inputHandlerSubmit(value) {
+    console.log(value);
+  }
+
   return (
     <div className="App">
-      <ChatHistory></ChatHistory>
+      <ChatHistory inputHandlerSubmit={inputHandlerSubmit}></ChatHistory>
     </div>
   );
 }
