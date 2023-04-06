fdce27a522611ef707e3c9f19e9c708cb83c50e8
CezarMarinescu <cmarinescu11@yahoo.com>
2023-04-06 20:07:38 +0300

    thirdcommit

--- src/App.js
+++ src/App.js
@@ -1,17 +1,26 @@
 import ErrorButton from './ErrorButton';
 import Header from './Header';
-import React from 'react';
-import { useState } from 'react';
 import './App.css';
 import Response from './Response';
 import controller from './modules/controller';
 import ChatHistory from './ChatHistory';
 import './ChatHistory.css';
+import React, { useState } from 'react';
+
+function inputHandlerSubmit(value) {
+  controller.getResponseMessage(value, 'user');
+  //setDataLength(controller.chatHistoryArray.length);
+}
 
 function App() {
-  const [dataLength, setDataLength] = useState(0);
-  controller.initApplication();
+  //const [dataLength, setDataLength] = useState(0);
+  //controller.initApplication();
 
+  const [displayError, setShowError] = useState(false);
+
+  const handleShowError = () => {
+    setShowError(true);
+  }
   // const myInput = 'mere bune';
   // const previous = ['pere', 'mere', 'repede'];
   // const letters = myInput.slice(-2);
@@ -22,21 +31,25 @@ function App() {
   //   console.error(err.message);
   // }
 
-  function inputHandlerSubmit(value) {
-    controller.getResponseMessage(value, 'user');
-    setDataLength(controller.chatHistoryArray.length);
-  }
-
   return (
     <div className="App">
+       <header className="App-header" style={{display: 'flex', alignItems: 'center', justifyContent: 'center'}}>
+      <button onClick={handleShowError}>Show Error</button>
+      {displayError && <ErrorButton />}
+      </header>
       <Header />
+      
       <ErrorButton />
       <ChatHistory
+      
         inputHandlerSubmit={inputHandlerSubmit}
         chatHistoryArray={controller.chatHistoryArray}
       ></ChatHistory>
+       
     </div>
+    
   );
+  
 }
-
 export default App;
+
